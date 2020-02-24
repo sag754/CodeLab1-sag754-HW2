f488853cb1cf7dd9884aae5203ecf1b83361d245
sag754 <56234238+sag754@users.noreply.github.com>
2020-02-24 12:44:52 -0500

    Tried to fix audio.

--- CodeLab1-sag754-HW3/Assets/Scripts/Coin.cs
+++ CodeLab1-sag754-HW3/Assets/Scripts/Coin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    AudioSource source;
+    public AudioSource source;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +18,7 @@ public class Coin : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) //If another GameObject with a 2D Collider on it hits this GameObject's collider
+    public void OnCollisionEnter2D(Collision2D collision) //If another GameObject with a 2D Collider on it hits this GameObject's collider
     {
         PlayerController.instance.coins++; //increase the player's score using the Singleton!
         Debug.Log("Score: " + PlayerController.instance.coins); //print the score to console, using the Singleton
