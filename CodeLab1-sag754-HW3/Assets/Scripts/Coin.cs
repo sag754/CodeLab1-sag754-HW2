6eb1b5a33c0c9f953a27b4a2a4b72373289a76a8
sag754 <56234238+sag754@users.noreply.github.com>
2020-02-24 12:50:27 -0500

    Tried to fix, but couldn't.

--- CodeLab1-sag754-HW3/Assets/Scripts/Coin.cs
+++ CodeLab1-sag754-HW3/Assets/Scripts/Coin.cs
@@ -18,7 +18,7 @@ public class Coin : MonoBehaviour
         
     }
 
-    public void OnCollisionEnter2D(Collision2D collision) //If another GameObject with a 2D Collider on it hits this GameObject's collider
+    private void OnCollisionEnter2D(Collision2D collision) //If another GameObject with a 2D Collider on it hits this GameObject's collider
     {
         PlayerController.instance.coins++; //increase the player's score using the Singleton!
         Debug.Log("Score: " + PlayerController.instance.coins); //print the score to console, using the Singleton
